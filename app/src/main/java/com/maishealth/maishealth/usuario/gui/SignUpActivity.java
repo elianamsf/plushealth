fa0431afb64f5e17aa59f3793d836e03bc622e8a
lucasrbk <torres_lucas2@hotmail.com>
2017-12-12 10:37:07 -0300

    Tela de cadastro e atualização do gradle e adição de strings no string.xml

---
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/SignUpActivity.java
@@ -0,0 +1,47 @@
+package com.maishealth.maishealth.usuario.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.maishealth.maishealth.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    private AutoCompleteTextView edtEmail, edtSenha, edtNome;
+    private EditText edtCpf, edtNasc;
+    private Spinner spinner;
+    private String[] listaSexo = {"Feminino", "Masculino", "Outros"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+        //ArrayAdapter é usado para preparar a lista que será usada no Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaSexo);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner = (Spinner)findViewById(R.id.spnSexo);
+        spinner.setAdapter(adapter);
+
+        //Metodo para quando um elemento do Spinner é selecionado()
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
