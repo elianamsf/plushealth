11021f5d88eed5233b087bfe5442cdf7d5ded60b
Edson Fagner <pg2006pe@hotmail.com>
2017-12-12 16:11:42 -0300

    Finalizado o ENUM com os estados.

--- app/src/main/java/com/maishealth/maishealth/usuario/dominio/EnumEstados.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/dominio/EnumEstados.java
@@ -1,7 +1,12 @@
 package com.maishealth.maishealth.usuario.dominio;
 
 public enum EnumEstados {
-    acre("AC"), Alagoas("AL");
+    ACRE("AC"), ALAGOAS("AL"), AMAPA("AP"), AMAZONAS("AM"), BAHIA("BA"), CEARA("CE"),
+    DISTRITO_FEDERAL("DF"), ESPIRITO_SANTO("ES"), GOIAS("GO"), MARANHAO("MA"), MATO_GROSSO("MT"),
+    MATO_GROSSO_DO_SUL("MS"), MINAS_GERAIS("MG"), PARA("PA"), PARAIBA("PB"),PARANA("PR"),
+    PERNAMBUCO("PE"), PIAUI("PI"), RIO_DE_JANEIRO("RJ"), RIO_GRANDE_DO_NORTE("RN"),
+    RIO_GRANDE_DO_SUL("RS"), RONDONIA("RO"), RORAIMA("RR"), SANTA_CATARINA("SC"), SAO_PAULO("SP"),
+    SERGIPE("SE"), TOCANTINS("TO");
 
     private String valor;
 
