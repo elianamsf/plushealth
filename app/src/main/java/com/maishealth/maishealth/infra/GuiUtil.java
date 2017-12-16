f898c7efb4dff1f09eddb316a1f0789cbf0d0bc0
Edson Fagner <pg2006pe@hotmail.com>
2017-12-16 00:12:06 -0300

    Criação da Classe GuiUtil. (Facilita mostrar msg para o usuário)

---
+++ app/src/main/java/com/maishealth/maishealth/infra/GuiUtil.java
@@ -0,0 +1,55 @@
+package com.maishealth.maishealth.infra;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.maishealth.maishealth.R;
+
+public class GuiUtil {
+
+
+    public static void myToast(Context context, String text, int time){
+        Toast.makeText(context, text, time).show();
+    }
+
+    public static void myToast(Context context, Exception e, int time){
+        GuiUtil.myToast(context, e.getMessage(), time);
+    }
+
+    public static void myToast(Context context, String text){
+        GuiUtil.myToast(context, text, Toast.LENGTH_LONG);
+    }
+
+    public static void myToast(Context context, Exception e){
+        GuiUtil.myToast(context, e.getMessage(), Toast.LENGTH_LONG);
+    }
+
+    public static void myToastShort(Context context, String text){
+        GuiUtil.myToast(context, text, Toast.LENGTH_SHORT);
+    }
+
+    public static void myToastShort(Context context, Exception e){
+        GuiUtil.myToast(context, e.getMessage(), Toast.LENGTH_SHORT);
+    }
+
+    public static void myAlertDialog(Context context, String text, String titulo){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
+        dialogo.setTitle(titulo);
+        dialogo.setMessage(text);
+        dialogo.setNeutralButton(R.string.dialogo_botao_ok, null);
+        dialogo.show();
+    }
+
+    public static void myAlertDialog(Context context, Exception e, String titulo){
+        GuiUtil.myAlertDialog(context, e.getMessage(), titulo);
+    }
+
+    public static void myAlertDialog(Context context, Exception e){
+        GuiUtil.myAlertDialog(context, e.getMessage(), context.getString(R.string.dialogo_titulo_erro));
+    }
+
+    public static void myAlertDialog(Context context, String text){
+        GuiUtil.myAlertDialog(context, text, context.getString(R.string.dialogo_titulo_erro));
+    }
+}
