9b1144b8c2085150c839378d50b28308bfc17c0a
Edson Fagner <pg2006pe@hotmail.com>
2017-12-18 20:12:28 -0300

    Inclusão no cadastro de médico e atualização na tela de cadastro.

--- app/src/main/java/com/maishealth/maishealth/usuario/gui/SignUpActivity.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/SignUpActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -21,8 +20,7 @@ import com.maishealth.maishealth.usuario.negocio.Servicos;
 import com.maishealth.maishealth.usuario.negocio.ValidaCadastro;
 
 public class SignUpActivity extends AppCompatActivity {
-    private AutoCompleteTextView edtEmail, edtSenha, edtNome;
-    private EditText edtCpf, edtNasc, edtCrm;
+    private EditText edtEmail, edtSenha, edtNome, edtCpf, edtNasc, edtCrm, edtEspec;
     private TextView edtRegiao;
     private Spinner spinnerSexo, spinnerRegiao;
     private String[] listaSexo = {"Feminino", "Masculino", "Outros"};
@@ -34,27 +32,34 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        edtEmail = findViewById(R.id.edtEmail3);
+        edtSenha = findViewById(R.id.edtSenha3);
+        edtNome = findViewById(R.id.edtNome3);
+        edtCpf = findViewById(R.id.edtCpf3);
+        edtCpf.addTextChangedListener(Mask.insert("###.###.###-##", edtCpf));
+        edtNasc = findViewById(R.id.edtNasc3);
+        edtNasc.addTextChangedListener(Mask.insert("##/##/####", edtNasc));
+        edtEspec = findViewById(R.id.edtEspec3);
         swUsuario = (Switch) findViewById(R.id.swUsuario);
         edtCrm = (EditText) findViewById(R.id.edtCRM3);
+        edtCrm.addTextChangedListener(Mask.insert("###.###.###.#", edtCrm));
         edtRegiao = (TextView) findViewById(R.id.textView7);
-        spinnerRegiao = (Spinner) findViewById(R.id.SpnRegiao3);
 
-        spinnerRegiao.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaEstados));
-        spinnerRegiao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
 
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         // Checa se o switch usuário é medico ou paciente
         // para setar visibilidade dos campos do cadastro do médico.
         swUsuario.setChecked(false);
+        spinnerRegiao = (Spinner) findViewById(R.id.SpnRegiao3);
+        spinnerRegiao.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaEstados));
+        spinnerRegiao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         swUsuario.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean bChecked) {
@@ -63,22 +68,17 @@ public class SignUpActivity extends AppCompatActivity {
                     edtCrm.setVisibility(View.VISIBLE);
                     edtRegiao.setVisibility(View.VISIBLE);
                     spinnerRegiao.setVisibility(View.VISIBLE);
+                    edtEspec.setVisibility(View.VISIBLE);
 
                 } else {
                     swUsuario.setText("Paciente");
                     edtCrm.setVisibility(View.INVISIBLE);
                     edtRegiao.setVisibility(View.INVISIBLE);
-                    spinnerRegiao.setVisibility(View.INVISIBLE);                }
+                    spinnerRegiao.setVisibility(View.INVISIBLE);
+                    edtEspec.setVisibility(View.INVISIBLE);
+                }
             }
         });
-
-        edtEmail = findViewById(R.id.edtEmail3);
-        edtSenha = findViewById(R.id.edtSenha3);
-        edtNome = findViewById(R.id.edtNome3);
-        edtCpf = findViewById(R.id.edtCpf3);
-        edtCpf.addTextChangedListener(Mask.insert("###.###.###-##", edtCpf));
-        edtNasc = findViewById(R.id.edtNasc3);
-        edtNasc.addTextChangedListener(Mask.insert("##/##/####", edtNasc));
 
         //ArrayAdapter é usado para preparar a lista que será usada no Spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaSexo);
@@ -89,18 +89,13 @@ public class SignUpActivity extends AppCompatActivity {
 
         //Metodo para quando um elemento do Spinner é selecionado()
         spinnerSexo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
-
     }
 
     public void validarCadatro(View view){
@@ -110,11 +105,25 @@ public class SignUpActivity extends AppCompatActivity {
         String sexo     = (String) spinnerSexo.getSelectedItem();
         String cpf      = edtCpf.getText().toString();
         String dataNasc = edtNasc.getText().toString();
+        String crm      = edtCrm.getText().toString();
         String estado     = (String) spinnerRegiao.getSelectedItem();
+        String especialidade = edtEspec.getText().toString();
 
 
         ValidaCadastro validaCadastro = new ValidaCadastro();
         boolean valido = true;
+
+        if(validaCadastro.isCampoVazio(especialidade) && swUsuario.isChecked()){
+            edtEspec.requestFocus();
+            edtEspec.setError("Especialidade inválida.");
+            valido = false;
+        }
+
+        if(!validaCadastro.isCrmValido(crm) && swUsuario.isChecked()){
+            edtCrm.requestFocus();
+            edtCrm.setError("CRM inválido.");
+            valido = false;
+        }
 
         if(!validaCadastro.isDataNascimento(dataNasc)){
             edtNasc.requestFocus();
@@ -146,11 +155,19 @@ public class SignUpActivity extends AppCompatActivity {
             valido = false;
         }
 
-        if(valido){
+        if(valido && !swUsuario.isChecked()){
             Servicos servicos = new Servicos(getApplicationContext());
             try{
                 servicos.cadastrarPaciente(email, senha, nome, sexo, dataNasc, cpf);
-                GuiUtil.myToast(this, "Cadastro realizado com sucesso");
+                GuiUtil.myToast(this, "Paciente cadastrado com sucesso");
+            } catch (Exception e) {
+                GuiUtil.myToast(this, e);
+            }
+        } else if(valido && swUsuario.isChecked()) {
+            Servicos servicos = new Servicos(getApplicationContext());
+            try{
+                servicos.cadastrarMedico(email, senha, nome, sexo, dataNasc, cpf, crm, estado, especialidade);
+                GuiUtil.myToast(this, "Médico cadastrado com sucesso");
             } catch (Exception e) {
                 GuiUtil.myToast(this, e);
             }
