c68938256f93e6f2f39db3c46051cdbfd5545cbf
Edson Fagner <pg2006pe@hotmail.com>
2017-12-20 19:43:33 -0300

    Adição de tabelas CONSULTA, DOENÇA, MEDICAMENTO, POSTO, PACIENTE-DOENCA, MÉDICO-POSTO e CONSULTA-MEDICAMENTO. Incremento na versão do banco.

--- app/src/main/java/com/maishealth/maishealth/infra/DataBase.java
+++ app/src/main/java/com/maishealth/maishealth/infra/DataBase.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper; // Cria banco de dados
 
-
-
 /**
  * Classe responsável por criar tabelas e o banco de dados
  */
 
 public class DataBase extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "dbmaishealth";
 
     //TABELA PESSOA
@@ -32,7 +30,6 @@ public class DataBase extends SQLiteOpenHelper {
     //TABELA PACIENTE
     public static final String TABELA_PACIENTE = "paciente";
     public static final String ID_PACIENTE = "id_paciente";
-    public static final String TABELA_HISTORICO = "historico";
     public static final String ID_EST_USUARIO_PA = "id_est_usuario";
     
     //TABELA MEDICO
@@ -43,11 +40,49 @@ public class DataBase extends SQLiteOpenHelper {
     public static final String ESPECIALIDADE = "especialidade";
     public static final String ID_EST_USUARIO_ME = "id_est_usuario";
 
-    private Context context;
+    //TABELA CONSULTA
+    public static final String TABELA_CONSULTA = "consulta";
+    public static final String CONSULTA_DATA = "data";
+    public static final String CONSULTA_DESCRICAO = "descricao";
+    public static final String ID_EST_PACIENTE_CON = "id_est_paciente";
+    public static final String ID_EST_MEDICO_CON = "id_est_medico";
+    public static final String CONSULTA_STATUS = "status";
+
+    //TABELA DOENCA CRONICA
+    public static final String TABELA_DOENCACRONICA = "doenca";
+    public static final String ID_DOENCA = "id_doenca";
+    public static final String DOENCA_NOME = "nome";
+    public static final String DOENCA_DESCRICAO = "descricao";
+
+    //TABELA MEDICAMENTO
+    public static final String TABELA_MEDICAMENTO = "medicamento";
+    public static final String ID_MEDICAMENTO = "id_medicamento";
+    public static final String MEDICAMENTO_NOME = "nome";
+
+    //TABELA POSTO
+    public static final String TABELA_POSTO = "posto";
+    public static final String ID_POSTO = "id_posto";
+    public static final String POSTO_NOME = "nome";
+    public static final String POSTO_LOCAL = "local";
+
+    //TABELA PACIENTE-DOENCA
+    public static final String TABELA_PACIENTE_DOENCA = "paciente_doenca";
+    public static final String ID_EST_PACIENTE_PA_DO = "id_paciente";
+    public static final String ID_EST_DOENCA_PA_DO = "id_doenca";
+
+    //TABELA MEDICO-POSTO
+    public static final String TABELA_MEDICO_POSTO = "medico_posto";
+    public static final String ID_EST_MEDICO_ME_POS = "id_medico";
+    public static final String ID_EST_POSTO_ME_POS = "id_posto";
+
+    //TABELA CONSULTA-MEDICAMENTO
+    public static final String TABELA_CONSULTA_MEDICAMENTO = "consulta_medicamento";
+    public static final String ID_EST_CONSULTA_CON_MEM = "id_consulta";
+    public static final String ID_EST_MEDICAMENTO_CON_MEM = "id_medicamento";
+
 
     public DataBase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
     }
 
     @Override
@@ -75,6 +110,39 @@ public class DataBase extends SQLiteOpenHelper {
                 ESTADO + " TEXT NOT NULL, " +
 				ESPECIALIDADE + " TEXT NOT NULL, " +
                 ID_EST_USUARIO_ME + " INTEGER);");
+
+        db.execSQL("CREATE TABLE " + TABELA_CONSULTA + " (" +
+                CONSULTA_DATA + " TEXT NOT NULL, " +
+                CONSULTA_DESCRICAO + " TEXT, " +
+                ID_EST_PACIENTE_CON + " INTEGER, " +
+                ID_EST_MEDICO_CON + " INTEGER, " +
+                CONSULTA_STATUS + " TEXT);");
+
+        db.execSQL("CREATE TABLE " + TABELA_DOENCACRONICA + " (" +
+                ID_DOENCA + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                DOENCA_NOME + " TEXT NOT NULL, " +
+                DOENCA_DESCRICAO + " TEXT NOT NULL);");
+
+        db.execSQL("CREATE TABLE " + TABELA_MEDICAMENTO + " (" +
+                ID_MEDICAMENTO + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MEDICAMENTO_NOME + " TEXT NOT NULL);");
+
+        db.execSQL("CREATE TABLE " + TABELA_POSTO + " (" +
+                ID_POSTO + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                POSTO_NOME + " TEXT NOT NULL, " +
+                POSTO_LOCAL + " TEXT NOT NULL);");
+
+        db.execSQL("CREATE TABLE " + TABELA_PACIENTE_DOENCA + " (" +
+                ID_EST_PACIENTE_PA_DO + " INTEGER, " +
+                ID_EST_DOENCA_PA_DO + " INTEGER);");
+
+        db.execSQL("CREATE TABLE " + TABELA_MEDICO_POSTO + " (" +
+                ID_EST_MEDICO_ME_POS + " INTEGER, " +
+                ID_EST_POSTO_ME_POS + " INTEGER);");
+
+        db.execSQL("CREATE TABLE " + TABELA_CONSULTA_MEDICAMENTO + " (" +
+                ID_EST_CONSULTA_CON_MEM + " INTEGER, " +
+                ID_EST_MEDICAMENTO_CON_MEM + " INTEGER);");
     }
 
     //Atualização da tabela
