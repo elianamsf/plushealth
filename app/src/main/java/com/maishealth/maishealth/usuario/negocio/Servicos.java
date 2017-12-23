b5978221509bd8d39e307e2f81817ca42341b497
Paulo Andrade <pcdajr@gmail.com>
2017-12-23 15:54:59 -0300

    Validação ESTADO/CRM (TELA DE CADASTRO MÉDICO)
    Feito o teste no emulador e verificado o DB do emulador virtual com sucesso.
    Todas as relações nas tabelas OK
    Não consegui fazer o mesmo teste no meu smartphone pois  ainda estou com problema pra verificar a pasta Data nele.

--- app/src/main/java/com/maishealth/maishealth/usuario/negocio/Servicos.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/negocio/Servicos.java
@@ -2,8 +2,10 @@ package com.maishealth.maishealth.usuario.negocio;
 
 import android.content.Context;
 
+import com.maishealth.maishealth.usuario.dominio.Medico;
 import com.maishealth.maishealth.usuario.dominio.Pessoa;
 import com.maishealth.maishealth.usuario.dominio.Usuario;
+import com.maishealth.maishealth.usuario.persistencia.MedicoDAO;
 import com.maishealth.maishealth.usuario.persistencia.PessoaDAO;
 import com.maishealth.maishealth.usuario.persistencia.UsuarioDAO;
 
@@ -15,14 +17,17 @@ public class Servicos {
     private ServicosPaciente servicosPaciente;
     private ServicosMedico servicosMedico;
     private PessoaDAO pessoaDAO;
+    private MedicoDAO medicoDAO;
 
     public Servicos(Context context) {
         usuarioDAO = new UsuarioDAO(context);
         pessoaDAO = new PessoaDAO(context);
+        medicoDAO = new MedicoDAO(context);
         servicosPessoa = new ServicosPessoa(context);
         servicosUsuario = new ServicosUsuario(context);
         servicosPaciente = new ServicosPaciente(context);
         servicosMedico = new ServicosMedico(context);
+
     }
 
     public long cadastrarPaciente(String email, String senha, String nome, String sexo, String dataNasc, String cpf, String tipoSangue) throws Exception {
@@ -45,9 +50,15 @@ public class Servicos {
     }
 
     public void cadastrarMedico(String email, String senha, String nome, String sexo, String dataNasc, String cpf, String tipoSangue, String crm, String estado, String especialidade) throws Exception {
+        Medico verificarRegiaoCrm = medicoDAO.getMedicoByRegiaoCrm(estado, crm);
+
+        if(verificarRegiaoCrm != null) {
+            throw new Exception(" Estado/CRM já cadastrado");
+        }
         try{
             long idUsuario = this.cadastrarPaciente(email, senha, nome, sexo, dataNasc, cpf, tipoSangue);
             servicosMedico.cadastrarMedico(crm, estado, especialidade, idUsuario);
+
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
