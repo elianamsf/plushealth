6617a7a73b6c26e695a24438bdbe9744cc78cc86
CGCavalcante <carol.gcavalcante@gmail.com>
2018-01-02 21:12:35 -0200

    Criação das telas para Marcação de Consultas: tela informar sintomas,  tela escolher especialidade, tela escolher dia, tela escolher horario e tela confirmação da consulta. Elaboração e padronização do Layout das telas de marcação: tela informar sintomas,  tela escolher especialidade, tela escolher dia, tela escolher horario e tela confirmação da consulta.

---
+++ app/src/main/java/com/maishealth/maishealth/EscolherEspecialidade.java
@@ -0,0 +1,13 @@
+package com.maishealth.maishealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EscolherEspecialidade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolher_especialidade);
+    }
+}
