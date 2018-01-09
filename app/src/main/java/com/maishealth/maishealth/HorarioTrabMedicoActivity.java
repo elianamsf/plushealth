9ac0f6140894b4765d3308d925e3866356845185
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2018-01-09 18:45:29 -0200

    Tela onde o médico define o seu horário de consulta e qtd vagas
    *Também tirei alguns warnings do xml da marcacao dos sintomas

---
+++ app/src/main/java/com/maishealth/maishealth/HorarioTrabMedicoActivity.java
@@ -0,0 +1,13 @@
+package com.maishealth.maishealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HorarioTrabMedicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_trab_medico);
+    }
+}
