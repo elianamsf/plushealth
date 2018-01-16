677263597abcf16bbbb3eb776b31af424cc2453d
WendersonLeonardo <32573898+wendersonleonardo@users.noreply.github.com>
2018-01-16 17:19:21 -0200

    Criado enum dos dias da semana para marcação de consulta.

---
+++ app/src/main/java/com/maishealth/maishealth/usuario/dominio/EnumDiasSemana.java
@@ -0,0 +1,28 @@
+package com.maishealth.maishealth.usuario.dominio;
+
+/**
+ * Created by Carol and Wenderson on 16/01/2018.
+ */
+
+public enum EnumDiasSemana {
+    SEGUNDA("Segunda-Feira"),TERCA("Terca-Feira"),QUARTA("Quarta-Feira"),QUINTA("Quinta-Feira"),SEXTA("Sexta-Feira")
+
+    private final String valor;
+
+    EnumDiasSemana(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public static String[] enumDiasSemanaLista(){
+        EnumDiasSemana[] listaDiasSemana = EnumDiasSemana.values();
+        String[] lista = new String[listaDiasSemana.length];
+        for (int i =0; i<listaDiasSemana.length;i++){
+            lista[i] = listaDiasSemana[i].getValor();
+        }
+        return  lista;
+    }
+}
