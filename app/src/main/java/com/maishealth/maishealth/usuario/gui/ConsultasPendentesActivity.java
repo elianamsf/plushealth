cc3c051e03dbc875a0257dac00e23cd60c6c7cc0
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2018-01-16 07:12:34 -0200

    List View para consultas pendentes e o histórico  referentes ao menu paciente

--- app/src/main/java/com/maishealth/maishealth/usuario/gui/ConsultasPendentesActivity.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/ConsultasPendentesActivity.java
@@ -4,16 +4,71 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.maishealth.maishealth.EscolherDiaDaConsultaActivity;
 import com.maishealth.maishealth.R;
 
 public class ConsultasPendentesActivity extends AppCompatActivity {
+
+    //isso aq dos nomes e descricoes foram feitas para os testes, mas vcs q tem q adptar para na vdd
+    //vir os nomes dos medicos
+    int[] IMAGES2 ={R.drawable.ic_event_38dp};
+    String[] NAMES2 = {"Doutora Kimbelly-Pediatra kkk","GANDHI","CopiChand"};
+    String[] DESCRIPTION2={"Data 16/03/2018","Data 17/08/2019","Data 23/05/2021"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultas_pendentes);
+
+        ListView listView=(ListView) findViewById(R.id.listView);
+            ConsultasPendentesActivity.CustomAdapter customAdapter=new ConsultasPendentesActivity.CustomAdapter();
+
+        listView.setAdapter(customAdapter);
     }
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            //return 0;
+            return IMAGES2.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        //@SuppressLint("ViewHolder")
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayout, null);
+
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            TextView textView_name = (TextView) view.findViewById(R.id.textView_name);
+            TextView textView_description = (TextView) view.findViewById(R.id.textView_descriptions);
+
+
+            //imageView.setImageResource(IMAGES2[i]);
+            textView_name.setText(NAMES2[i]);
+            textView_description.setText(DESCRIPTION2[i]);
+
+            return view;
+        }
+    }
+    //falta metodo para mostrar detalhes da consulta pendente(pegar o cliq da pessoa e mostrar)
+
     public void voltarMenuPac(View view){
         Intent intent= new Intent(ConsultasPendentesActivity.this, MenuPaciente.class);
         startActivity(intent);
--- app/src/main/java/com/maishealth/maishealth/usuario/gui/MeuHistoricoActivity.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/MeuHistoricoActivity.java
@@ -8,46 +8,86 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.maishealth.maishealth.Historico;
 import com.maishealth.maishealth.R;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MeuHistoricoActivity extends AppCompatActivity {
+
+    int[] IMAGES2 = {R.drawable.ic_event_38dp};
+    String[] NAMES2 = {"Doutor Felipe-Clínico geral", "GANDHI", "CopiChand"};
+    String[] DESCRIPTION2 = {"Data 11/02/2015", "Data 17/08/2019", "Data 23/05/2021"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meu_historico);
 
-        //getSupport....
-        //HistoricoFragment fragment =((HistoricoFragment) getSupportFragmentManager()).findFragmentByTag("mainFrag");
-        //if (fragment==null){
-         //   fragment=new HistoricoFragment();
-           // FragmentTransaction ft =getSupportFragmentManager().beginTransaction();
-            //ft replace Adiciona o fragmente que criamos manualmente ao nosso relativeLayout que declaramos no MeuHistorico.xml
-            //ft.replace(R.id.rl_fragment_historico,fragment,"mainFrag");
-            //ft commit Realiza as alterações do nosso FragmentTransaction.
-            //ft.commit();
+        ListView listView = (ListView) findViewById(R.id.listView);
+        MeuHistoricoActivity.CustomAdapter customAdapter = new MeuHistoricoActivity.CustomAdapter();
+
+        listView.setAdapter(customAdapter);
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            //return 0;
+            return IMAGES2.length;
         }
-    //}
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        //@SuppressLint("ViewHolder")
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayout, null);
+
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            TextView textView_name = (TextView) view.findViewById(R.id.textView_name);
+            TextView textView_description = (TextView) view.findViewById(R.id.textView_descriptions);
+
+
+            //imageView.setImageResource(IMAGES2[i]);
+            textView_name.setText(NAMES2[i]);
+            textView_description.setText(DESCRIPTION2[i]);
+
+            return view;
+        }
+    }
     //mudança de tela - retornando para a tela de paciente
-    public void voltarMenuPacI(View view){
-        Intent intent= new Intent(MeuHistoricoActivity.this, MenuPaciente.class);
+    public void voltarMenuPacI(View view) {
+        Intent intent = new Intent(MeuHistoricoActivity.this, MenuPaciente.class);
         startActivity(intent);
         finish();
 
     }
-    //metodo que vai retornar uma lista de historico(consultas no caso)
-    public List<Historico>getListHistorico(){
-        List<Historico>mlist=new ArrayList<>();
-        for (int i=0; i<20 ;i++){
-            Historico historico= new Historico("Data"+i, "Clinico geral", "Pessoa de nome"+i);
+
+    //metodo que vai retornar uma lista de historico(consultas no caso) - isso foi do fragment...
+    public List<Historico> getListHistorico() {
+        List<Historico> mlist = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Historico historico = new Historico("Data" + i, "Clinico geral", "Pessoa de nome" + i);
             mlist.add(historico);
         }
         return mlist;
+        }
     }
-
-}
