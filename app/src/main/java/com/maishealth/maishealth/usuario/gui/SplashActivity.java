d4265b9e69d4c1932f9c4532c18cd8394451328f
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2018-01-20 21:47:26 -0200

    Tela de inicialização do app

---
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.maishealth.maishealth.usuario.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.maishealth.maishealth.R;
+
+/**
+ * <h1>SplashActivity</h1>
+ * Actitivy responsavel por implementar as funcionalidades da tela de abertura (splash screen)
+ * durante a inicialização do aplicativo.
+ */
+
+public class SplashActivity extends Activity implements Runnable {
+
+    /**
+     * O método onCreate() tem a funcionalidade de setar o layout: activity_splash e
+     * usa um handler para essa activity.
+     *
+     * @param savedInstanceState Objeto da classe Bundle que contem o estado anterior da activity
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    /**
+     * O metodo run() tem a funcionalidade de mudar dessa activity para a LogInActivity e
+     * finaliza essa activity.
+     *
+     * @see LoginActivity
+     */
+
+    public void run() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
