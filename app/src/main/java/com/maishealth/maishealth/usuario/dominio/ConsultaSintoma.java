0d888cc782e6f5c7f7f2ea9e189d30c168318a58
Eliana Maria <elianamsfcontatos@gmail.com>
2018-01-24 23:40:41 -0300

    Serviços e persistencia para paciente marcar consulta

---
+++ app/src/main/java/com/maishealth/maishealth/usuario/dominio/ConsultaSintoma.java
@@ -0,0 +1,24 @@
+package com.maishealth.maishealth.usuario.dominio;
+
+
+public class ConsultaSintoma {
+
+    private long idConsultaSintoma;
+    private String nomeSintoma;
+
+    public long getIdConsultaSintoma() {
+        return idConsultaSintoma;
+    }
+
+    public void setIdConsultaSintoma(long idConsultaSintoma) {
+        this.idConsultaSintoma = idConsultaSintoma;
+    }
+
+    public String getNomeSintoma() {
+        return nomeSintoma;
+    }
+
+    public void setNomeSintoma(String nomeSintoma) {
+        this.nomeSintoma = nomeSintoma;
+    }
+}
